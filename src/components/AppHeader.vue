8cace8558b54a5051d09864628a7add411b47c2b
Thales Mariano <thales.mariano125@gmail.com>
2024-07-22 12:02:43 -0300

    Header updated

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,30 @@
 <script setup>
+	import { ref } from 'vue'
 	import { RouterLink } from 'vue-router'
+	import ProfileHeader from '@/components/ProfileHeader.vue'
+
+	const isAuth = ref(true)
+
 </script>
 
 <template>
-	<header class="w-full p-3 flex justify-between items-center bg-neutral-700">
+	<header class="w-full px-6 py-4 flex justify-between items-center bg-neutral-700 shadow-md">
 		<div class="flex items-center">
 			<h1 class="text-white font-bold text-3xl">Tropa dos Animes</h1>
 
 			<nav class="flex items-center gap-x-5 ml-12">
-				<RouterLink to="/" class="font-semibold text-white">Home</RouterLink>
-			  	<RouterLink to="/categorys" class="font-semibold text-white">Categorys</RouterLink>
+				<RouterLink to="/" class="font-semibold text-white hover:underline">Home</RouterLink>
+			  	<RouterLink to="/categorys" class="font-semibold text-white hover:underline">Categorys</RouterLink>
 			</nav>
 		</div>
 
-		<div class="space-x-3">
-			<RouterLink to="/sign" class="px-5 py-2 border rounded text-white">Entrar</RouterLink>
+		<div class="flex gap-3 items-center">
+
+			<div v-if="!isAuth" class="space-x-3">
+				<RouterLink to="/signin" class="px-5 py-2 border rounded text-white hover:bg-neutral-300 hover:text-black hover:border-black transition-all">Entrar</RouterLink>
+			</div>
+
+			<ProfileHeader v-if="isAuth"/>
 		</div>
 	  </header>
 </template>
---
+++ src/components/ProfileHeader.vue
@@ -0,0 +1,68 @@
+<script setup>
+	import { ref } from 'vue'
+	import { RouterLink } from 'vue-router'
+
+	const isOpened = ref(false)
+	const caretIcon = ref(null)
+
+	function openProfileLinks(){
+		isOpened.value = !isOpened.value
+		caretIcon.value.classList.toggle('rotate-180')
+	}
+</script>
+
+<template>
+	<div class="relative">
+		<button @click="openProfileLinks" class="flex gap-3 items-center">
+			<div class="w-10 h-10 rounded-full bg-cyan-200">
+					
+			</div>
+			<img ref="caretIcon" class="w-5 duration-300 transition-all" src="@/assets/icons/caret-down-fill.svg">
+		</button>
+
+		<Transition
+			enter-active-class="animate__animated animate__fadeIn"
+ 			leave-active-class="animate__animated animate__fadeOut"
+		>
+			<div v-if="isOpened" class="absolute -bottom-[19rem] -left-24 w-44 h-72 bg-neutral-800 rounded-md shadow-lg">
+				<nav class="link-container relative">
+					<ul>
+						<li class="border-b-[.55px] border-neutral-300 z-[999]">
+							<RouterLink to="/profile" class="px-2 py-1 text-white flex gap-x-3 items-center hover:bg-neutral-900 transition-all rounded-t-md">
+								<img class="w-5" src="@/assets/icons/person-fill.svg">
+								Perfil
+							</RouterLink>
+						</li>
+						<li class="border-b-[.55px] border-neutral-300 z-[999]">
+							<RouterLink to="/configurations" class="px-2 py-1 text-white flex gap-x-3 items-center hover:bg-neutral-900 transition-all">
+								<img class="w-5" src="@/assets/icons/gear-wide.svg">
+								Configurações
+							</RouterLink>
+						</li>
+						<li class="border-b-[.55px] border-neutral-300 z-[999]">
+							<RouterLink to="/logout" class="px-2 py-1 text-white flex gap-x-3 items-center hover:bg-neutral-900 transition-all">
+								<img class="w-5" src="@/assets/icons/box-arrow-right.svg">
+								Sair
+							</RouterLink>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</Transition>
+	</div>
+</template>
+
+<style scoped>
+	.link-container::before {
+		content: '';
+		width: 20px;
+		height: 20px;
+		background-color: #262626;
+		position: absolute;
+		top: -9px;
+		right: 7px;
+		border-radius: 3px;
+		transform: rotateZ(-45deg);
+		clip-path: polygon(0 0, 100% 0, 100% 100%, 50% 50%);
+	}
+</style>
